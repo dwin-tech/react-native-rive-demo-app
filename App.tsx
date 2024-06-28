18e58a99e0e32abbef073a6219531dadf71548a0
Varuj2002 <varuzhan.hovsepyan.dwin@gmail.com>
2024-06-28 14:42:36 +0400

    React Native Rive Demo

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Rive from 'rive-react-native';
+
+const App = () => {
+  return (
+    <Rive
+      url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+      artboardName="Avatar 1"
+      stateMachineName="avatar"
+      style={{width: 400, height: 400}}
+    />
+  );
+};
+
+export default App;
